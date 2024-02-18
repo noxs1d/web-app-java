2e0852d7d40bea23a42da94541f880c0422a7fbe
noxs1d <117533743+noxs1d@users.noreply.github.com>
2024-02-18 22:29:27 +0500

    Update WebappApplication.java

--- src/main/java/com/example/webapp/WebappApplication.java
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -11,11 +11,14 @@ public class WebappApplication {
 
 	public static void main(String[] args) {
 		final String db_url="jdbc:mysql://localhost:3306/db_example";
-		try{
-			Connection connection= DriverManager.getConnection(db_url,"root","Tsukuyomy7@");
-			Statement statement=connection.createStatement();
-			if(!connection.isClosed()){
-				System.out.println("Is Connected");
+		try(Connection connection= DriverManager.getConnection(db_url,"root","Tsukuyomy7@");
+			Statement statement=connection.createStatement();){
+
+			ResultSet sr=statement.executeQuery("select * from users");
+			while( sr.next()){
+				int i=sr.getInt(1);
+				String str =sr.getString(2);
+				System.out.println(i+") "+str);
 			}
 		}
 		catch (SQLException e) {
