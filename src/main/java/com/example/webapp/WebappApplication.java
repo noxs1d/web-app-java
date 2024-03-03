0d393eef51ed3b978613fbdf812ce6321a87178f
noxs1d <117533743+noxs1d@users.noreply.github.com>
2024-03-03 15:21:59 +0500

    -mysql-connector

--- src/main/java/com/example/webapp/WebappApplication.java
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -10,20 +10,8 @@ public class WebappApplication {
 
 
 	public static void main(String[] args) {
-		final String db_url="jdbc:mysql://localhost:3306/db_example";
-		try(Connection connection= DriverManager.getConnection(db_url,"root","Tsukuyomy7@");
-			Statement statement=connection.createStatement();){
 
-			ResultSet sr=statement.executeQuery("select * from users");
-			while( sr.next()){
-				int i=sr.getInt(1);
-				String str =sr.getString(2);
-				System.out.println(i+") "+str);
-			}
-		}
-		catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
         SpringApplication.run(WebappApplication.class, args);
 
 	}
